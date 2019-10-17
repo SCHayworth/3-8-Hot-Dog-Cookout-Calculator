20c49a16a85fcabb8562c394a1131220510d1ccc
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-17 09:59:03 -0700

    Update hot-dog-cookout-calc.py

--- hot-dog-cookout-calc.py
+++ hot-dog-cookout-calc.py
@@ -1,7 +1,7 @@
 # Program 3-8 Hot Dog Cookout Calculator
 # Shaun Hayworth
 # CIS 2
-# 10-16-2019
+# 10-17-2019
 # Original source code and revision history can be found at https://github.com/SCHayworth/3-8-Hot-Dog-Cookout-Calculator
 
 # Program calculates the minimum number of packages of hot dogs and buns required for a cookout, given a number of people
@@ -24,26 +24,12 @@ dogs_per_person = int(input('Number of hot dogs per person: '))
 # Calculate the total hot dogs needed
 dogs_needed = attending * dogs_per_person
 
-# Calculate the minimum number of hot dog packages needed
-dog_minimum = dogs_needed // DOG_PACKAGE
+# Check whether the amount of hot dogs needed is evenly divisible by 10
+if dogs_needed % DOG_PACKAGE = 0:
 
-# Calculate the total amount of hot dogs on hand
-total_dogs = dog_minimum * DOG_PACKAGE
+    # if yes, divide by the number of hotdogs in the package
+    minimum_dogs = dogs_needed / DOG_PACKAGE
+else:
 
-# Calculate the leftover hot dogs
-leftover_dogs = dogs_needed - total_dogs
-
-# Calculate the minimum number of bun packages needed
-bun_minimum = dogs_needed // BUN_PACKAGE
-
-# Calculate the total number of buns on hand
-total_buns = bun_minimum * BUN_PACKAGE
-
-# Calculate the number of leftover buns
-leftover_buns = dogs_needed - total_buns
-
-# Display the minimum packages of hot dogs and buns needed, and how many of each will be left over
-print(f'Minimum packages of hot dogs needed: {dog_minimum}')
-print(f'Minimum npackages of buns needed: {bun_minimum}')
-print(f'Hot dogs left over: {leftover_dogs}')
-print(f'Buns left over: {leftover_buns}')
+    # If not, floor divide by the number of packages and add 1
+    minimum_dogs = (dogs_needed // DOG_PACKAGE) + 1
