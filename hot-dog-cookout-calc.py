2cefbc0e0e5a9808ab52c12a16b3af0f8d82ae44
Shaun Hayworth <thismoderndeath@gmail.com>
2019-10-17 10:27:46 -0700

    Corrected serious logic errors

--- hot-dog-cookout-calc.py
+++ hot-dog-cookout-calc.py
@@ -24,12 +24,36 @@ dogs_per_person = int(input('Number of hot dogs per person: '))
 # Calculate the total hot dogs needed
 dogs_needed = attending * dogs_per_person
 
-# Check whether the amount of hot dogs needed is evenly divisible by 10
-if dogs_needed % DOG_PACKAGE = 0:
-
-    # if yes, divide by the number of hotdogs in the package
+# Check whether the amount of hot dogs needed is evenly divisible by the number of dogs in a package
+# if yes, divide by the number of hotdogs in the package
+if dogs_needed % DOG_PACKAGE == 0:
     minimum_dogs = dogs_needed / DOG_PACKAGE
-else:
 
-    # If not, floor divide by the number of packages and add 1
+# If not, floor divide by the number in a package of hot dogs and add 1
+else:
     minimum_dogs = (dogs_needed // DOG_PACKAGE) + 1
+
+# Check whether the amount of buns needed is evenly divisible by the number of buns in a packages
+# If yes, divide by the number of buns in a package
+if dogs_needed % BUN_PACKAGE == 0:
+    minimum_buns = dogs_needed / BUN_PACKAGE
+
+# If not, floor divide by the number of buns in a package and add 1
+else:
+    minimum_buns = (dogs_needed // BUN_PACKAGE) + 1
+
+# Calculate the total number of hot dogs
+total_dogs = minimum_dogs * DOG_PACKAGE
+
+# Calculate the total number of Buns
+total_buns = minimum_buns * BUN_PACKAGE
+
+# Calculate how many hot dogs and buns will be left over.
+leftover_dogs = total_dogs - dogs_needed
+leftover_buns = total_buns - dogs_needed
+
+# Display the minimum number of hot dog packages, bun packages, and how many of each will be left leftover
+print(f'Minimum packages of hot dogs needed: {minimum_dogs}')
+print(f'Minimum packages of buns needed: {minimum_buns}')
+print(f'Hot dogs left over: {leftover_dogs}')
+print(f'Buns left over: {leftover_buns}')  
